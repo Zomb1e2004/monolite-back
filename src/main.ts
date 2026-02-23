9a530771a41d61357f05aa1d4bde73afe4b7add3
Zomb1e2004 <dr3amerz0mb1e97@gmail.com>
2026-02-23 11:34:06 -0500

    feat: add AllExceptionsFilter for centralized exception handling

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ResponseInterceptor } from './shared/interceptors/response.interceptor';
+import { AllExceptionsFilter } from './shared/filters/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('/api');
+
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   await app.listen(process.env.PORT ?? 3000);
 }
---
+++ src/shared/filters/exceptions.filter.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    let message: string | string[];
+
+    if (exception instanceof HttpException) {
+      const res = exception.getResponse();
+      if (typeof res === 'string') {
+        message = res;
+      } else if (typeof res === 'object' && res['message']) {
+        message = res['message'];
+      } else {
+        message = 'Error';
+      }
+    } else {
+      message = 'Internal server error';
+    }
+
+    response.status(status).json({
+      success: false,
+      data: null,
+      message,
+    });
+  }
+}
